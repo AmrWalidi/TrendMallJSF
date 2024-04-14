3b9a004189e0c36d8cd55de8e16f38de7c21aa71
Amr Walidi <amr.nawaf@gmail.com>
2024-04-14 17:10:52 +0300

    Delete ManagdeBean class and add the upload function to the ImageDAO class

--- src/java/servlet/ImageServlet.java
+++ src/java/servlet/ImageServlet.java
@@ -11,27 +11,30 @@ import jakarta.servlet.http.HttpServletResponse;
 import java.io.IOException;
 import java.io.OutputStream;
 import java.sql.SQLException;
+import java.util.logging.Level;
+import java.util.logging.Logger;
 
 @WebServlet("/image")
 public class ImageServlet extends HttpServlet {
 
     @Override
     protected void doGet(HttpServletRequest request, HttpServletResponse response) {
-        String imageId = request.getParameter("id"); // Assuming the image ID is passed as a parameter
         byte[] imageData = null;
         try {
+            String imageID = request.getParameter("id");
             Class.forName("org.postgresql.Driver");
             Connection conn = DriverManager.getConnection("jdbc:postgresql://localhost:5432/JSFexampleDB", "postgres", "AmrF.C.B");
-            Statement statement = conn.createStatement();
-            String sql = "SELECT content FROM images WHERE id = " + Integer.valueOf(imageId);
-            ResultSet rs = statement.executeQuery(sql);
-            if (rs.next()) {
-                imageData = rs.getBytes("content"); // Specify the correct MIME type for your image
+            Statement st = conn.createStatement();
+            String sql = "SELECT content FROM images WHERE id = " + Integer.valueOf(imageID);
+            ResultSet rs = st.executeQuery(sql);
+            while (rs.next()) {
+                imageData = rs.getBytes("content");
             }
             response.setContentType("image/png");
-            OutputStream out = response.getOutputStream();
-            out.write(imageData);
-        } catch (IOException | ClassNotFoundException | NumberFormatException | SQLException e) {
+            try (OutputStream out = response.getOutputStream()) {
+                out.write(imageData);
+            }
+        } catch (IOException | ClassNotFoundException | SQLException e) {
             e.getMessage();
         }
     }
