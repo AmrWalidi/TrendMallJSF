feba8ac01451dd08e902f8a39dbb35c9e8f5140e
Amr Walidi <amr.nawaf@gmail.com>
2024-05-15 19:55:41 +0300

    rewrite getUrunler method and add urunkategorileri method

--- src/java/dao/UrunDAO.java
+++ src/java/dao/UrunDAO.java
@@ -111,27 +111,31 @@ public class UrunDAO {
         return adet;
     }
 
+    private List<Kategori> getUrunKategorileri(int urun_id) {
+        List<Kategori> kategoriler = new ArrayList<>();
+        try {
+            Statement st = this.getConn().createStatement();
+            String query = "SELECT * FROM kategori WHERE id in (SELECT kategori_id FROM urun_kategori WHERE urun_id = " + urun_id + ")";
+            ResultSet rs = st.executeQuery(query);
+            while (rs.next()) {
+                kategoriler.add(new Kategori(rs.getInt("id"), rs.getString("ad")));
+            }
+        } catch (SQLException ex) {
+            System.out.println(ex.getMessage());
+        }
+        return kategoriler;
+    }
+
     public List<Urun> getUrunler(int counter) {
         List<Urun> urunler = new ArrayList<>();
         try {
-            Statement st1 = this.getConn().createStatement();
-            String query1 = "SELECT * FROM urun offset " + (counter - 1) * 5 + " limit 5";
-            ResultSet rs1 = st1.executeQuery(query1);
-            while (rs1.next()) {
-                Statement st2 = this.getConn().createStatement();
-                String query2 = "SELECT * FROM urun_kategori where urun_id = " + rs1.getInt("id");
-                ResultSet rs2 = st2.executeQuery(query2);
-                List<Kategori> kategoriler = new ArrayList<>();
-                while (rs2.next()) {
-                    Statement st3 = this.getConn().createStatement();
-                    String query3 = "SELECT * FROM kategori where id = " + rs2.getInt("kategori_id");
-                    ResultSet rs3 = st3.executeQuery(query3);
-                    while (rs3.next()) {
-                        kategoriler.add(new Kategori(rs3.getInt("id"), rs3.getString("ad")));
-                    }
-                }
-                Satici s = getSaticiDao().getSatici(rs1.getInt("satici_id"));
-                urunler.add(new Urun(rs1.getInt("id"), rs1.getString("ad"), s, kategoriler, rs1.getInt("miktar"), rs1.getFloat("fiyat")));
+            Statement st = this.getConn().createStatement();
+            String query = "SELECT * FROM urun offset " + (counter - 1) * 5 + " limit 5";
+            ResultSet rs = st.executeQuery(query);
+            while (rs.next()) {
+                List<Kategori> kategoriler = this.getUrunKategorileri(rs.getInt("id"));
+                Satici s = getSaticiDao().getSatici(rs.getInt("satici_id"));
+                urunler.add(new Urun(rs.getInt("id"), rs.getString("ad"), s, kategoriler, rs.getInt("miktar"), rs.getFloat("fiyat")));
             }
         } catch (SQLException ex) {
             System.out.println(ex.getMessage());
@@ -163,24 +167,13 @@ public class UrunDAO {
     public List<Urun> getUrunler(int counter, String urun) {
         List<Urun> urunler = new ArrayList<>();
         try {
-            Statement st1 = this.getConn().createStatement();
-            String query1 = "SELECT * FROM urun WHERE ad like '%" + urun + "%'offset " + (counter - 1) * 5 + " limit 5";
-            ResultSet rs1 = st1.executeQuery(query1);
-            while (rs1.next()) {
-                Statement st2 = this.getConn().createStatement();
-                String query2 = "SELECT * FROM urun_kategori where urun_id = " + rs1.getInt("id");
-                ResultSet rs2 = st2.executeQuery(query2);
-                List<Kategori> kategoriler = new ArrayList<>();
-                while (rs2.next()) {
-                    Statement st3 = this.getConn().createStatement();
-                    String query3 = "SELECT * FROM kategori where id = " + rs2.getInt("kategori_id");
-                    ResultSet rs3 = st3.executeQuery(query3);
-                    while (rs3.next()) {
-                        kategoriler.add(new Kategori(rs3.getInt("id"), rs3.getString("ad")));
-                    }
-                }
-                Satici s = getSaticiDao().getSatici(rs1.getInt("satici_id"));
-                urunler.add(new Urun(rs1.getInt("id"), rs1.getString("ad"), s, kategoriler, rs1.getInt("miktar"), rs1.getFloat("fiyat")));
+            Statement st = this.getConn().createStatement();
+            String query = "SELECT * FROM urun WHERE ad like '%" + urun + "%' offset " + (counter - 1) * 5 + " limit 5";
+            ResultSet rs = st.executeQuery(query);
+            while (rs.next()) {
+                List<Kategori> kategoriler = this.getUrunKategorileri(rs.getInt("id"));
+                Satici s = getSaticiDao().getSatici(rs.getInt("satici_id"));
+                urunler.add(new Urun(rs.getInt("id"), rs.getString("ad"), s, kategoriler, rs.getInt("miktar"), rs.getFloat("fiyat")));
             }
         } catch (SQLException ex) {
             System.out.println(ex.getMessage());
